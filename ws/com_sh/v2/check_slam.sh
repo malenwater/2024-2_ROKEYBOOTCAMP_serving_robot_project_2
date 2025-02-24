1e343b8bf9506ae29f8d98f9ccfb0de66ace78bf
malenwater <128200788+malenwater@users.noreply.github.com>
2025-02-24 16:39:02 +0900

    v2 slam sh 변경

--- ws/com_sh/v2/check_slam.sh
+++ ws/com_sh/v2/check_slam.sh
@@ -1,1 +1,4 @@
 ros2 param get /slam_toolbox resolution
+ros2 param get /slam_toolbox max_laser_range
+ros2 param get /slam_toolbox distance_variance_penalty
+ros2 param get /slam_toolbox angle_variance_penalty
