9212c9473397a9d3c89e375d0636d4169d98884e
leekante <94979429+leekante@users.noreply.github.com>
2025-02-24 17:17:10 +0900

    v2 완성!
    문제점 _ 갈때는 잘그리는데, 올떄 맵이 틀어짐
    생각 _ 아마, 속도의 문제이지 않을까..?

--- ws/com_sh/v2/check_nav2.sh
+++ ws/com_sh/v2/check_nav2.sh
@@ -1,1 +1,4 @@
-ros2 param get /controller_server controller_frequency
+ros2 param get /controller_server general_goal_checker.yaw_goal_tolerance
+
+ros2 param get /controller_server general_goal_checker.xy_goal_tolerance
+ros2 param get /local_costmap/local_costmap inflation_layer.inflation_radius
